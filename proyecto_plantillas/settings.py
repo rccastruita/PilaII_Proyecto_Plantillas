06468d07f76ce79b2f5c99e640edad7fd8580407
Rafael Carmona Castruita <rccastruita@gmail.com>
2021-03-07 21:02:38 -0600

    Added README.md

--- proyecto_plantillas/settings.py
+++ proyecto_plantillas/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fontawesome-free',
+    'fontawesome-free', # Vector icons
 ]
 
 MIDDLEWARE = [
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Serve local static files on deploy
 ]
 
 ROOT_URLCONF = 'proyecto_plantillas.urls'
