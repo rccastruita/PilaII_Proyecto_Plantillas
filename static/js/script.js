f64fda7a90489b7ca007b593b4a016b79003726b
Rafael Carmona Castruita <rccastruita@gmail.com>
2021-04-02 15:10:26 -0600

    Fixed hover bugs on mobile browser.

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,4 @@
 // Display the dropdown menu when clicking the hamburger button
 $("#navbar-dropdown-button").on("click", function(event) {
     $(".dropdown").toggleClass("dropdown--visible");
-    $("#navbar-dropdown-button").toggleClass("navbar__button--active");
 });
