e3f2cc705fe8f3cb4e116dba316fb5e079d2430d
Rafael Carmona Castruita <rccastruita@gmail.com>
2021-04-07 01:35:16 -0500

    Added custom user fields to admin site TODO: - User forms - User views  - Login  - Logout  - Register  - Update  - Password change and reset

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,51 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 from . import models as MyModels
 
+
 # Register your models here.
-admin.site.register(MyModels.User)
+class UserAdmin(BaseUserAdmin):
+    model = MyModels.User
+
+    list_display = (
+        'username',
+        'email',
+        'city',
+        'in_mailing_list',
+    )
+    
+    fieldsets = (
+        (None, {
+            'fields': (
+                'username',
+                'password',
+            )
+        }),
+        ("Personal info", {
+            'fields': (
+                'email',
+                'first_name',
+                'last_name',
+                'city', 
+                'in_mailing_list',
+            )
+        }),
+        ("Important dates", {
+            'fields': (
+                'date_joined',
+                'last_login',
+            )
+        }),
+        ("Permissions", {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            )
+        })
+    )
+
+admin.site.register(MyModels.User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    city = models.ForeignKey("stores.City", null=True, on_delete=models.SET_NULL)
+    city = models.ForeignKey("stores.City", null=True, blank=True, on_delete=models.SET_NULL)
     in_mailing_list = models.BooleanField(default=True)
