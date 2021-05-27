6236bdbd61d517535488fa44ad1bfc22dbfc0747
Rafael Carmona Castruita <rccastruita@gmail.com>
2021-05-27 16:48:41 -0500

    Added bind mount volume to docker container Added dark mode preview Added links for Product CRUD

--- static/js/script.js
+++ static/js/script.js
@@ -5,4 +5,16 @@ $("#navbar-dropdown-button").on("click", function(event) {
 
 $(".comment--clickable").on("click", function(event) {
     //alert("Comment clicked");
-})
+});
+
+// Switch to dark mode when the checkbox is clicked
+$("#dark-mode-switch").change(function() {
+    if(this.checked) {
+        $(":root").css("--color-neutral-900", "white");
+        $(":root").css("--color-neutral-000", "black");
+    }
+    else {
+        $(":root").css("--color-neutral-900", "black");
+        $(":root").css("--color-neutral-000", "white");
+    }
+});
